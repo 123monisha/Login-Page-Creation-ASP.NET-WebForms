e41b1eb0e1967459ec16f51c6f4ebc985c5f9999
Monisha Ishwar <monishamoger15@gmail.com>
2025-09-20 21:50:24 +0530

    Add project files.

---
+++ Loginn_Page.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Loginn_Page.aspx.cs" Inherits="Login_Page_Creation.Loginn_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Page</title>
+     <link href="bootstrap.css" rel="stylesheet" />
+    <style type="text/css">
+        
+    </style>
+    <script>>
+    function validate() {
+        let email = document.getElementById("txtEmail").value;
+        let pwd = document.getElementById("txtPwd").value;
+
+        if (email.trim().length == 0) {
+            document.getElementById("lblMsg").innerHTML = "Enter Email id";
+            return false;
+        }
+        if (pwd.trim().length == 0) {
+            document.getElementById("lblMsg").innerHTML = "Enter Password";
+            return false;
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <center> <div style="width:50%;border:solid thin blue; ">
+            <br />
+            <div><center><h1 style="text-align:center">Login Page</h1></center></div>
+            <hr /><br />
+            <div style="text-align:center">WelCome:
+                <asp:Label ID="lblUser" runat="server" Text="" CssClass="form-Control"></asp:Label>
+            </div>
+            <br /><br />
+            <div style="text-align:center">
+                Username:
+                <asp:TextBox ID="txtName1" runat="server" cssClass="form-Control" Width="335px"></asp:TextBox>
+            </div>
+            <br /><br />
+            <div style="text-align:center">
+                Password:
+                <asp:TextBox ID="txtPwd" runat="server" CssClass="form-Control" Width="327px"></asp:TextBox>
+            </div>
+            <br /><br />
+            <div style="text-align:center">
+                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" Width="202px" OnClientClick="return validate()" />
+            </div><br />
+            <br />
+            <div style="text-align:center" >
+                <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+            </div>
+            <br />
+            <br />
+        </div></center>
+    </form>
+</body>
+</html>
---
+++ Loginn_Page.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Web.UI;
+
+namespace Login_Page_Creation
+{
+    public partial class Loginn_Page : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=shopping; Integrated Security=true";
+
+            if (Session["isloggedin"] != null && (bool)Session["isloggedin"])
+            {
+                lblUser.Text = Session["Name"].ToString();
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            // open connection first
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+
+            // check session status
+            if (Session["isloggedin"] == null || !(bool)Session["isloggedin"])
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT Cid, Name, Password FROM Customers WHERE Name=@name";
+                cmd.Parameters.AddWithValue("@name", txtName1.Text.Trim());
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    if (dr["password"].ToString() == txtPwd.Text.Trim())
+                    {
+                        Session["isloggedin"] = true;
+                        Session["cid"] = dr["cid"];
+                        Session["Name"] = dr["Name"];
+                        lblUser.Text = dr["Name"].ToString();
+                        lblMsg.Text = "Your Are Successfully logged in dear!!!";
+                        lblMsg.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        lblUser.Text = "Invalid password";
+                        lblUser.ForeColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    lblUser.Text = "User not found";
+                    lblUser.ForeColor = Color.Red;
+                }
+
+                dr.Close();
+            }
+            else
+            {
+                lblUser.Text = "Already logged in as " + Session["cname"];
+                lblUser.ForeColor = Color.Yellow;
+            }
+
+            con.Close();
+        }
+    }
+}
